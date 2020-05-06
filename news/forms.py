c3ee33ff0ef34120ef47e40f29a19a64e5bc403d
Lili Rassh <lili.rassh@gmail.com>
2020-05-06 22:18:46 +0300

    CRUD-операций в разделе новостей

--- news/forms.py
+++ news/forms.py
@@ -6,3 +6,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'about', 'content', 'author', 'image', 'source')
+
+
+class PostForm2(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'about', 'content', 'author', 'source')
