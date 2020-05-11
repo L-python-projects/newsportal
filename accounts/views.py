c0c7b121524b34e20c9d840337d1868103b337ac
Lili Rassh <lili.rassh@gmail.com>
2020-05-11 22:28:46 +0300

    Добавлена регистрация

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hashlib import md5
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def sign_up(request):
-    data = {'title': 'Регистрация'}
-    return render(request, 'accounts/sign_up.html', context=data)
+    data = dict()
+    if request.method == 'GET':
+        data['title'] = 'Регистрация'
+        return render(request, 'accounts/sign_up.html', context=data)
+    elif request.method == 'POST':
+        # Извлечение данных из формы:
+        login_x = request.POST.get('login')
+        pass1_x = request.POST.get('pass1')
+        pass2_x = request.POST.get('pass2')
+        email_x = request.POST.get('email')
+
+        # Каскад проверок данных (валидация):
+        if pass1_x != pass2_x:
+            data['color_x'] = 'red'
+            data['report_x'] = 'Пароли не совпадают!'
+        elif pass1_x == '':
+            # Остальные проверки:
+            pass
+        else:
+            # Техническая проверка:
+            data['login'] = login_x
+            data['pass1'] = pass1_x
+            data['pass2'] = pass2_x
+            data['email'] = email_x
+
+            # Добавление пользователя в базу БД:
+            user = User.objects.create_user(login_x, email_x, pass1_x)
+            user.save()
+
+            # Формирование отчета:
+            data['title'] = 'Отчет о регистрации:'
+            if user is None:
+                data['color_x'] = 'red'
+                data['report_x'] = 'В регистрации отказано!'
+            else:
+                data['color_x'] = 'green'
+                data['report_x'] = 'Регистрация успешно завершена!'
+
+        return render(request, 'accounts/reports.html', context=data)
 
 
 def sign_in(request):
-    data = {'title': 'Вход'}
-    return render(request, 'accounts/sign_in.html', context=data)
+    data = dict()
+    if request.method == 'GET':
+        data['title'] = 'Вход'
+        return render(request, 'accounts/sign_in.html', context=data)
+    elif request.method == 'POST':
+        #
+        login_x = request.POST.get('login')
+        pass1_x = request.POST.get('pass1')
+
+        # Проверка подлинности:
+        user = authenticate(request, username=login_x, password=pass1_x)
+        if user is None:
+            data['color_x'] = 'red'
+            data['report_x'] = 'Пользователь не найден!!'
+            data['title'] = 'Отчет о авторизации'
+            return render(request, 'accounts/reports.html', context=data)
+        else:
+            login(request, user)
+            return redirect('/home')
 
 
 def sign_out(request):
